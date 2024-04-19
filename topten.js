922edbd74231475b21afc90dc69b472a7b620454
ASpeedyNiNjA <christrombly001@hotmail.com>
2024-04-19 15:31:38 -0400

    Added 2nd section for 7 day old articles

--- topten.js
+++ topten.js
@@ -41,6 +41,46 @@ function displayResults(json) {
 }
 
 
+// Section Two ~ 7 days old 
+// The URL for the top ten API at nytimes.com
+const baseUrl2 = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?';
+// The full URL for generating JSON using variables
+let url2 = `${baseUrl2}api-key=${key}`;
+// Console logging the URL to verify it's ok
+console.log(url2);
+
+// Fetch top articles from the previous seven days {Note #1}
+fetch(url2).then(result2 => {
+	return result2.json();
+}).then(json2 => {
+	displayResults2(json2);
+});
+
+// Need a variable that targets something in the DOM
+const list2 = document.querySelector('#seven');
+
+function displayResults2(json2) {
+	console.log(json2);
+	articles2 = json2.results;
+	console.log(articles2); // {Note #2}
+	articles2.forEach((article) => {
+		console.log(article);
+
+		const header2 = document.createElement('h3');
+		const listItem2 = document.createElement('li');
+		const anchor2 = document.createElement('a');
+		const para2 = document.createElement('p');
+
+		header2.textContent = article.title;
+		para2.textContent = article.abstract;
+		anchor2.href = article.url;
+		anchor2.textContent = article.url;
+		list2.appendChild(listItem2);
+		listItem2.appendChild(header2);
+		listItem2.appendChild(anchor2);
+		listItem2.appendChild(para2);
+	});
+}
 
 
 
